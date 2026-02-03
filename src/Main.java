d82d92bee162cbe6d66938bd92a4789c03625333
KevnAp <apariciodkevin@Gmail.com>
2026-02-02 20:29:47 -0600

    built options for user with do while loop to check for validity.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        // Start off the program by asking user if they want their data in metric or imperial system."
+        options();
+
+    }
+
+    // Functions
+    // Check for input validity
+    static void options(){
+        // Initialize scanner for user input.
+        Scanner sc = new Scanner(System.in);
+
+        // variable choice holds the user selection.
+        int choice;
+
+        do {
+            System.out.println("Choose your option (1 for US metrics, 2 for general metrics");
+            choice = sc.nextInt();
+        } while (choice < 1 || choice > 2);
 
     }
 }
